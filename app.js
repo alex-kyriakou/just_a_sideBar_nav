6f76d23576409771d162b37e69a68c3c203a9a83
Alexandros <alexandr.kyriakou@gmail.com>
2022-02-07 22:36:04 +0200

    active tab

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const shrinkBtn = document.querySelector(".shrink-btn");
 const search = document.querySelector(".search");
 const input = document.getElementById("input");
+const sidebar_links = document.querySelectorAll(".sidebar-links a");
+const active_tab = document.querySelector(".active-tab ");
+
+let activeIndex;
 
 shrinkBtn.addEventListener("click", () => {
   document.body.classList.toggle("shrink");
@@ -17,3 +21,19 @@ search.addEventListener("click", () => {
   document.body.classList.remove("shrink");
   search.lastElementChild.focus();
 });
+
+function moveActiveTab() {
+  let topPosition = activeIndex * 58 + 2.5;
+  active_tab.style.top = `${topPosition}px`;
+}
+
+function changeLink() {
+  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
+  this.classList.add("active");
+
+  activeIndex = this.dataset.active;
+
+  moveActiveTab();
+}
+
+sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
